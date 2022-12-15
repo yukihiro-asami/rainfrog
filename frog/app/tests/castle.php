6a945ecfc66abf43cff6395e1e9441bb048f7cf7
yukihiro-asami <npbasami@gmail.com>
2022-12-15 18:27:22 +0900

    change name

--- frog/app/tests/castle.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Test_Hoge extends TestCase
-{
-    public function test_hoge1()
-    {
-        echo \castle\secret_box_key_generate();
-    }
-}
---
+++ frog/app/tests/frog.php
@@ -0,0 +1,9 @@
+<?php
+class Tests_Castle extends TestCase
+{
+    public function test_hoge1()
+    {
+        echo 'hoge' . PHP_EOL;
+        echo 'hoge';
+    }
+}
