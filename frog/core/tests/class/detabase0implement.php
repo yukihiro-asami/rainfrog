8075cb36dfeaac50ebff8059389253cb17438478
yukihiro-asami <npbasami@gmail.com>
2022-12-28 17:07:07 +0900

    add database0implement test

--- frog/core/tests/class/detabase0implement.php
+++ frog/core/tests/class/detabase0implement.php
@@ -1,7 +1,4 @@
-<?php /** @noinspection PhpUnused */
-/** @noinspection DuplicatedCode */
-
-/** @noinspection SqlNoDataSourceInspection SqlDialectInspection */
+<?php /** @noinspection PhpConditionAlreadyCheckedInspection PhpUnused DuplicatedCode SqlNoDataSourceInspection SqlDialectInspection*/
 
 use function castle\database_implement;
 
@@ -183,8 +180,117 @@ EOF;
         $database0implement->query($sql)->execute();
     }
 
-    function test_bind()
+    function test_bind_param_params_quote()
     {
+        foreach ([FRG_DB_INSTANCE_PRIMARY, FRG_DB_INSTANCE_SECONDARY, FRG_DB_INSTANCE_TERTIARY, FRG_DB_INSTANCE_QUATERNARY] as $database_implement_id)
+        {
+            $database0implement = database_implement($database_implement_id);
 
+            $sql = <<<EOF
+DROP TABLE IF EXISTS `test0field`;
+EOF;
+            $database0implement->query($sql)->execute();
+
+            $sql = <<<EOF
+CREATE TABLE `TRADESYSTEM`.`test0field`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `field_char` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `field_int` int(10) NULL DEFAULT NULL,
+  `field_float` double NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `key`(`field_char`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+EOF;
+            $database0implement->query($sql)->execute();
+
+            $char_value = '文字列string';
+            $field_int = 126;
+            $field_float = 123.555543;
+
+            $sql = <<<EOF
+INSERT INTO `TRADESYSTEM`.`test0field` SET field_char = :char_value,`field_int` = :int_value, `field_float` = :float_value
+EOF;
+            $database0implement->query($sql)
+                ->bind('char_value', $char_value)
+                ->bind('int_value', $field_int)
+                ->bind('float_value', $field_float)
+                ->execute();
+
+            $actual = $database0implement->find_by('test0field', 'field_char', $char_value);
+            $expect = [
+                0 =>
+                    [
+                        'id' => 1,
+                        'field_char' => '文字列string',
+                        'field_int' => 126,
+                        'field_float' => 123.555543
+                    ]
+            ];
+            $this->assertEquals($expect, $actual);
+
+            $char_value = 'key1';
+            $field_int = 128;
+            $field_float = 3.0000003;
+
+            $sql = <<<EOF
+INSERT INTO `TRADESYSTEM`.`test0field` SET field_char = :char_value,`field_int` = :int_value, `field_float` = :float_value
+EOF;
+            $database0implement->query($sql)
+                ->param('char_value', $char_value)
+                ->param('int_value', $field_int)
+                ->param('float_value', $field_float)
+                ->execute();
+
+            $actual = $database0implement->find_by('test0field', 'field_char', $char_value);
+            $expect = [
+                0 =>
+                    [
+                        'id' => 2,
+                        'field_char' => 'key1',
+                        'field_int' => 128,
+                        'field_float' => 3.0000003
+                    ]
+            ];
+            $this->assertEquals($expect, $actual);
+
+            $char_value = 'key2';
+            $field_int = 128000;
+            $field_float = 314.0000003;
+
+            $params = ['char_value' => $char_value,
+            'int_value' => $field_int,
+            'float_value' => $field_float];
+
+            $sql = <<<EOF
+INSERT INTO `TRADESYSTEM`.`test0field` SET field_char = :char_value,`field_int` = :int_value, `field_float` = :float_value
+EOF;
+            $database0implement->query($sql)
+                ->params($params)
+                ->execute();
+
+            $actual = $database0implement->find_by('test0field', 'field_char', $char_value);
+            $expect = [
+                0 =>
+                    [
+                        'id' => 3,
+                        'field_char' => 'key2',
+                        'field_int' => 128000,
+                        'field_float' => 314.0000003
+                    ]
+            ];
+            $this->assertEquals($expect, $actual);
+
+            $test_string = "Co'mpl''ex \"st'\"ring";
+            $actual = $database0implement->quote($test_string);
+            $expect = <<<EOF
+'Co\'mpl\'\'ex \"st\'\"ring'
+EOF;
+            $this->assertEquals($expect, $actual);
+
+            $sql = <<<EOF
+DROP TABLE IF EXISTS `test0field`;
+EOF;
+            $database0implement->query($sql)->execute();
+        }
     }
 }
