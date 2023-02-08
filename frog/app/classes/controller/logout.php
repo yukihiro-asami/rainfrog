4c837ecc8f6a7e19c00ce15114206bcfaf022b0b
yukihiro-asami <npbasami@gmail.com>
2023-02-08 15:09:57 +0900

    delete files

--- frog/app/classes/controller/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Controller_Logout extends Controller
-{
-    function get_index(string $hoge = ' no param')
-    {
-        Auth::logout();
-        return Response::forge('ok');
-    }
-}
