5c4efeace9e8fa0db2ff33f134386c965b237c81
yukihiro-asami <npbasami@gmail.com>
2023-11-21 15:09:00 +0900

    fix bug:  set $vals['captured_cookie_values']　empty array

--- frog/loader/closures/execute_task.php
+++ frog/loader/closures/execute_task.php
@@ -6,7 +6,9 @@ use PHPUnit\SebastianBergmann\CodeCoverage\Report\PHP;
 return function (array &$vals) : string
 {
     global $__task_class_name;
+    $vals['captured_cookie_values'] = [];
     set_credential(new Credential0implement());
+    $vals['set_credential'] = 'done';
     $task_program_path =  $vals['app_classes_dir'] . 'task/'.  mb_strtolower($__task_class_name) . '.php';
     if (file_exists($task_program_path) === true)
     {
