e88f9f19955a604e10a407f47af3935c20fad416
yukihiro-asami <npbasami@gmail.com>
2023-12-21 05:52:34 +0900

    update error and exception handler

--- frog/loader/closures/register_error_handler.php
+++ frog/loader/closures/register_error_handler.php
@@ -5,19 +5,30 @@ return function (array &$vals) : string
 {
     $callback = function ($errno, $errstr, $errfile, $errline) use ($vals)
 {
+    global $__db_logs, $__status, $__headers, $__mode;
     $path = $vals['views_dir'] . 'castle/__503__.php';
     $params = [
-        'message' => $errno . $errstr,
+        'message' => $errstr . '(error no: ' . $errno . ')',
         'file' => $errfile . '(' . $errline . ')',
     ];
     set_status(FRG_HTTP_STATUS_CODE_503_SERVICE_UNAVAILABLE);
     send_status_line();
     send_headers();
     echo view($params, $path);
+    $vals['db_log'] = $__db_logs;
+    $vals['http_response_status_code'] = $__status;
+    $vals['http_headers'] = $__headers;
+    $vals['mode'] = $__mode;
     $vals['end_with'] = "error_handler";
     $vals['message'] = $errno . $errstr;
     $vals['file'] = $errfile . '(' . $errline . ')';
-    file_put_contents($vals['syslog_dir']  . $vals['syslog_id'] . '.json', json_encode($vals, JSON_PRETTY_PRINT));
+    /** @noinspection DuplicatedCode */
+    if ($vals['is_syslog_enabled'])
+    {
+        file_put_contents($vals['syslog_dir']  . $vals['syslog_id'] . '.json', json_encode($vals, JSON_PRETTY_PRINT));
+        if ($__mode === FRG_MODE_TASK)
+            echo 'syslog path: ' . '"' . $vals['syslog_dir']  . $vals['syslog_id'] . '.json"' . PHP_EOL;
+    }
     die;
 };
     set_error_handler($callback);
--- frog/loader/closures/register_exception_handler.php
+++ frog/loader/closures/register_exception_handler.php
@@ -4,6 +4,7 @@ return function (array &$vals) : string
 {
     $callback = function (\Throwable $throwable) use ($vals)
 {
+    global $__db_logs, $__status, $__headers, $__mode;
     $path = $vals['views_dir'] . 'castle/__503__.php';
     $params = [
         'message' => $throwable->getMessage(),
@@ -13,10 +14,20 @@ return function (array &$vals) : string
     send_status_line();
     send_headers();
     echo view($params, $path);
+    $vals['db_log'] = $__db_logs;
+    $vals['http_response_status_code'] = $__status;
+    $vals['http_headers'] = $__headers;
+    $vals['mode'] = $__mode;
     $vals['end_with'] = "exception_handler";
     $vals['message'] = $throwable->getMessage();
     $vals['file'] = $throwable->getFile() . '(' . $throwable->getLine() . ')';
-    file_put_contents($vals['syslog_dir']  . $vals['syslog_id'] . '.json', json_encode($vals, JSON_PRETTY_PRINT));
+    /** @noinspection DuplicatedCode */
+    if ($vals['is_syslog_enabled'])
+    {
+        file_put_contents($vals['syslog_dir']  . $vals['syslog_id'] . '.json', json_encode($vals, JSON_PRETTY_PRINT));
+        if ($__mode === FRG_MODE_TASK)
+            echo 'syslog path: ' . '"' . $vals['syslog_dir']  . $vals['syslog_id'] . '.json"' . PHP_EOL;
+    }
     die;
 };
     set_exception_handler($callback);
